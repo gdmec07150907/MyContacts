41a50397dc598a6d146ce3bc6b8814523d2119cd
gdmec07150907 <570941644@qq.com>
2017-01-01 13:16:05 +0800

    Initial commit

---
+++ app/src/main/java/com/example/chenpc/mycontact/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.chenpc.mycontact;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.home_pc.mycontacts.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
